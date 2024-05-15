f615721dd5f6ac99ac9d623990a929ebbb5cfe4a
shenliuming <ceekayshen@foxmail.com>
2024-05-15 10:40:41 +0800

    修改项目的打包配置

--- touat.sh
+++ touat.sh
@@ -3,6 +3,6 @@ cd `dirname $0`
 
 server=be01
 
-mvn -T 1C clean package -Dmaven.test.skip=true -pl=be-water-server -am \
+mvn -T 1C clean package -pl=be-water-server -am  -Dmaven.test.skip=true \
 && rsync -avz be-water-server/target/ddm-api-server.jar root@${server}:/opt/docker/ddm/api/app/ddm-api-server.jar \
 && ssh root@${server} 'docker restart ddm-api-server && docker logs -f --tail=10 ddm-api-server'
